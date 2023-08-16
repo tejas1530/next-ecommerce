7bd8442d7c8ddd2344b52cae59247e397d21afa4
tejas1530 <bhatttejas30@gmail.com>
2023-08-17 00:14:57 +0530

    " first commit "

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+       <Head>
+        <title>Next-ECommerce</title>
+        <meta property="og:title" content="My page title" key="title" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
