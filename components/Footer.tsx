01eefb28839cf36cf75f820e63bcb8d7ed9a8ce5
tejas1530 <bhatttejas30@gmail.com>
2023-08-17 02:25:05 +0530

    " footer done "

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from "../public/1146.jpg"
+import logo from "../public/logo.png"
 import Image from 'next/image'
 
 type Props = {}
@@ -17,46 +17,47 @@ const Footer = (props: Props) => {
     <hr className="mt-2 md:overflow-hidden" />
     <div className="grid grid-cols-2 my-3 text-center md:grid-cols-4">
         <div className="flex flex-col leading-7 items-start ml-20">
-            <h1 className="text-blue-500 text-lg font-semibold">Product</h1>
-            <p> <a className="text-gray-500/80 hover:text-cyan-600" href="#">Features</a> </p>
-            <p> <a className="text-gray-500/80 hover:text-cyan-600" href="#">Integrations</a> </p>
-            <p> <a className="text-gray-500/80 hover:text-cyan-600" href="#">Shopify Integrations</a> </p>
-            <p> <a className="text-gray-500/80 hover:text-cyan-600" href="#">Pricing</a> </p>
-            <p> <a className="text-gray-500/80 hover:text-cyan-600" href="#">Chrome Extensions</a> </p>
+            <h1 className="text-pink-500 text-lg font-semibold">Product</h1>
+            <p> <a className="text-gray-500/80 hover:text-pink-500" href="#">Features</a> </p>
+            <p> <a className="text-gray-500/80 hover:text-pink-500" href="#">Integrations</a> </p>
+            <p> <a className="text-gray-500/80 hover:text-pink-500" href="#">Shopify Integrations</a> </p>
+            <p> <a className="text-gray-500/80 hover:text-pink-500" href="#">Pricing</a> </p>
+            <p> <a className="text-gray-500/80 hover:text-pink-500" href="#">Chrome Extensions</a> </p>
         </div>
         <div className="flex flex-col leading-7">
-            <h1 className="text-blue-500 text-lg font-semibold">Company</h1>
-            <p> <a className="text-gray-500/80 hover:text-cyan-600" href="#">Terms</a> </p>
-            <p> <a className="text-gray-500/80 hover:text-cyan-600" href="#">Customers</a> </p>
-            <p> <a className="text-gray-500/80 hover:text-cyan-600" href="#">Blog</a> </p>
-            <p> <a className="text-gray-500/80 hover:text-cyan-600" href="#">Terms of Service</a> </p>
-            <p> <a className="text-gray-500/80 hover:text-cyan-600" href="#">Privacy Policy</a> </p>
+            <h1 className="text-pink-500 text-lg font-semibold">Company</h1>
+            <p> <a className="text-gray-500/80 hover:text-pink-500" href="#">Terms</a> </p>
+            <p> <a className="text-gray-500/80 hover:text-pink-500" href="#">Customers</a> </p>
+            <p> <a className="text-gray-500/80 hover:text-pink-500" href="#">Blog</a> </p>
+            <p> <a className="text-gray-500/80 hover:text-pink-500" href="#">Terms of Service</a> </p>
+            <p> <a className="text-gray-500/80 hover:text-pink-500" href="#">Privacy Policy</a> </p>
         </div>
         <div className="flex flex-col my-6 leading-7 md:my-0">
-            <h1 className="text-blue-500 text-lg font-semibold">Support</h1>
-            <p> <a className="text-gray-500/80 hover:text-cyan-600" href="#">Community</a> </p>
-            <p> <a className="text-gray-500/80 hover:text-cyan-600" href="#">Support Customers</a> </p>
-            <p> <a className="text-gray-500/80 hover:text-cyan-600" href="#">Documentations</a> </p>
-            <p> <a className="text-gray-500/80 hover:text-cyan-600" href="#">Service Providers</a> </p>
-            <p> <a className="text-gray-500/80 hover:text-cyan-600" href="#">Email Contact</a> </p>
+            <h1 className="text-pink-500 text-lg font-semibold">Support</h1>
+            <p> <a className="text-gray-500/80 hover:text-pink-500" href="#">Community</a> </p>
+            <p> <a className="text-gray-500/80 hover:text-pink-500" href="#">Support Customers</a> </p>
+            <p> <a className="text-gray-500/80 hover:text-pink-500" href="#">Documentations</a> </p>
+            <p> <a className="text-gray-500/80 hover:text-pink-500" href="#">Service Providers</a> </p>
+            <p> <a className="text-gray-500/80 hover:text-pink-500" href="#">Email Contact</a> </p>
         </div>
         <div className="flex flex-col my-6 leading-7 md:my-0">
-            <h1 className="text-blue-500 text-lg font-semibold">Contact Us</h1>
-            <p> <a className="text-gray-500/80 hover:text-cyan-600" href="#">Support@admin.com</a> </p>
-            <p> <a className="text-gray-500/80 hover:text-cyan-600" href="#">(+91)700-788-67-00</a> </p>
+            <h1 className="text-pink-500 text-lg font-semibold">Contact Us</h1>
+            <p> <a className="text-gray-500/80 hover:text-pink-500" href="#">Support@admin.com</a> </p>
+            <p> <a className="text-gray-500/80 hover:text-pink-500" href="#">(+91)700-788-67-00</a> </p>
         </div>
     </div>
     <hr className="mt-2 md:invisible" />
     <div className="flex flex-col items-center m-5 md:flex-row md:justify-between md:mx-28"> 
     <Image
-    src={logo}
-    alt='logo image'
-    height={150}
-    width={200}
-    />
-        <div className="mt-4 text-gray-500 font-semibold"> <a className="hover:text-blue-500 tracking-wide" href="/">Here
-            </a> <strong>|</strong> <a className="hover:text-blue-500 tracking-normal" href="/">Scroll on top</a>
-        I</div>
+        className='md:h-14 md:w-14 h-8 w-8   '
+        src={logo}
+        alt='logo'
+        // height={10}
+        // width={200}
+        />
+        <div className="mt-4 text-gray-500 font-semibold"> <a className="hover:text-pink-500 tracking-wide" href="/">Here
+            </a> <strong>|</strong> <a className="hover:text-pink-500 tracking-normal" href="/">Scroll on Top</a>
+        </div>
     </div>
 </div>
 </div>
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,12 +2,13 @@ import Image from 'next/image'
 import React from 'react'
 import logo from "../public/logo.png"
 import Link from 'next/link'
+import {FiShoppingCart}from"react-icons/fi"
 type Props = {}
 
 const NavBar = (props: Props) => {
   return (
     <header className='flex justify-between space-y-3 flex-col items-center md:flex-row ' >
-      <div className="logo mx-5 flex items-center w-screen sm:w-fit sm:p-5 justify-around ">
+      <div className="logo mx-5 pt-2 flex items-center w-screen sm:w-fit sm:p-3.5 justify-around ">
         <Image
         className='md:h-14 md:w-14 h-8 w-8   '
         src={logo}
@@ -15,7 +16,7 @@ const NavBar = (props: Props) => {
         // height={10}
         // width={200}
         />
-        <div className='sm:hidden'  >Cart</div>
+        <div className='sm:hidden'  ><FiShoppingCart className="text-xl" /></div>
       </div>
       <div className="nav">
          <ul className='flex space-x-9   ' >
@@ -25,8 +26,8 @@ const NavBar = (props: Props) => {
             <Link href={'/'} ><li>item 4</li></Link>
          </ul>
       </div>
-      <div className="cart md:mx-7 hidden sm:block ">
-        <button>cart</button>
+      <div className="cart md:mx-7 hidden sm:block  ">
+        <FiShoppingCart className="text-3xl"  />
       </div>
     </header>
   )
