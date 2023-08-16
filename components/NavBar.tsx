0d0ed2dedfd585b92a15a607ed1956f87b288731
tejas1530 <bhatttejas30@gmail.com>
2023-08-17 02:02:07 +0530

    " navbar defined "

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,29 +1,31 @@
 import Image from 'next/image'
 import React from 'react'
-import logo from "../public/1146.jpg"
+import logo from "../public/logo.png"
+import Link from 'next/link'
 type Props = {}
 
 const NavBar = (props: Props) => {
   return (
-    <header className='flex justify-between ' >
-      <div className="logo">
+    <header className='flex justify-between space-y-3 flex-col items-center md:flex-row ' >
+      <div className="logo mx-5 flex items-center w-screen sm:w-fit sm:p-5 justify-around ">
         <Image
-        className='h-14'
+        className='md:h-14 md:w-14 h-8 w-8   '
         src={logo}
         alt='logo'
-        height={10}
-        width={200}
+        // height={10}
+        // width={200}
         />
+        <div className='sm:hidden'  >Cart</div>
       </div>
       <div className="nav">
-         <ul className='flex' >
-            <li>item 1</li>
-            <li>item 2</li>
-            <li>item 3</li>
-            <li>item 4</li>
+         <ul className='flex space-x-9   ' >
+            <Link href={'/'} ><li>item 1</li></Link>
+            <Link href={'/'} ><li>item 2</li></Link>
+            <Link href={'/'} ><li>item 3</li></Link>
+            <Link href={'/'} ><li>item 4</li></Link>
          </ul>
       </div>
-      <div className="cart">
+      <div className="cart md:mx-7 hidden sm:block ">
         <button>cart</button>
       </div>
     </header>
