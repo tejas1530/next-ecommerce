83a1bdbaa7bd80c1e7ba3a3ddf60af868c05e663
tejas1530 <bhatttejas30@gmail.com>
2023-08-17 00:32:41 +0530

    " created pages  "

---
+++ pages/order.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const order = (props: Props) => {
+  return (
+    <div>order</div>
+  )
+}
+
+export default order
---
+++ pages/tshirt.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const tshirt = (props: Props) => {
+  return (
+    <div>tshirt</div>
+  )
+}
+
+export default tshirt
