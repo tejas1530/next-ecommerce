a23812ccd9bf196c5239b4f5443df7f6435394f8
tejas1530 <bhatttejas30@gmail.com>
2023-08-20 02:53:07 +0530

    " cart functionality "

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,10 +3,52 @@ import React, { useRef } from "react";
 import logo from "../public/logo.png";
 import Link from "next/link";
 import { FiShoppingCart } from "react-icons/fi";
-import { AiFillCloseCircle ,AiOutlineArrowRight,AiOutlineMinusCircle,AiOutlinePlusCircle} from "react-icons/ai";
+import {
+	AiFillCloseCircle,
+	AiOutlineArrowRight,
+	AiOutlineMinusCircle,
+	AiOutlinePlusCircle,
+} from "react-icons/ai";
 type Props = {};
 
-const NavBar = (props: Props) => {
+interface ProductCart {
+	itemCode: number;
+	qty: number;
+	price: number;
+	name: string;
+	size: string;
+	variant: string;
+}
+interface CartProps {
+	cart: { [itemCode: number]: ProductCart };
+	removeFromCart: (
+		itemCode: number,
+		qty: number,
+		price: number,
+		name: string,
+		size: string,
+		variant: string
+	) => void;
+	addToCart: (
+		itemCode: number,
+		qty: number,
+		price: number,
+		name: string,
+		size: string,
+		variant: string
+	) => void;
+	subTotal: number;
+	clearCart: () => void;
+}
+
+const NavBar = (props: CartProps) => {
+	// console.log(
+	// 	props.cart,
+	// 	props.addToCart,
+	// 	props.clearCart,
+	// 	props.removeFromCart,
+	// 	props.subTotal
+	// );
 	const ref = React.useRef<HTMLInputElement>(null);
 	const toggleCart = () => {
 		if (ref.current?.classList.contains("translate-x-full")) {
@@ -75,44 +117,44 @@ const NavBar = (props: Props) => {
 					<AiFillCloseCircle />{" "}
 				</span>
 				<ol className="list-decimal space-y-3 font-semibold ">
-					<li>
-						<div className="flex" >
-              <div className="w-2/3 font-semibold " >
-                  t shirt wear the code
-              </div>
-              <div className="flex font-semibold items-center justify-center w-1/3  " >  <AiOutlineMinusCircle className="mx-1 text-2xl" />1 <AiOutlinePlusCircle className="mx-1 text-2xl" />
-              </div>
-            </div>
-					</li>
-          <li>
-						<div className="flex" >
-              <div className="w-2/3 font-semibold " >
-                  t shirt wear the code
-              </div>
-              <div className="flex font-semibold items-center justify-center w-1/3  " >1</div>
-            </div>
-					</li>
-          <li>
-						<div className="flex" >
-              <div className="w-2/3 font-semibold " >
-                  t shirt wear the code
-              </div>
-              <div className="flex font-semibold items-center justify-center w-1/3  " >1</div>
-            </div>
-					</li>
-          <li>
-						<div className="flex" >
-              <div className="w-2/3 font-semibold " >
-                  t shirt wear the code
-              </div>
-              <div className="flex font-semibold items-center justify-center w-1/3  " >1</div>
-            </div>
-					</li>
+					{
+						Object.keys(props.cart).length===0 &&
+						<div 
+						className="text-center text-xl py-24 font-thin "
+						>No items in your cart </div>
+					}
+				{
+					 Object.keys(props.cart).map((k:any) => {
+						// const itemCode = parseInt(k, 10); // Parse the string key to a number
+						// const cartItem = props.cart;
+						return (
+						  <li key={k}>
+							<div className="flex">
+							  <div className="w-2/3 font-semibold">{props.cart[k].name}</div>
+							  <div className="flex font-semibold items-center justify-center w-1/3">
+								<AiOutlineMinusCircle  onClick={()=>props.removeFromCart(k,1,props.cart[k].price,props.cart[k].name,props.cart[k].size,props.cart[k].variant)} className="mx-1 text-2xl" />
+								{props.cart[k].qty}
+								<AiOutlinePlusCircle  onClick={()=>props.addToCart(k,1,props.cart[k].price,props.cart[k].name,props.cart[k].size,props.cart[k].variant)} className="mx-1 text-2xl" />
+							  </div>
+							</div>
+						  </li>
+						);
+					  })
+				}
+					
 				</ol>
-        <div className="flex justify-center items-center flex-col space-y-4 sm:space-y-0 sm:space-x-3 sm:flex-row mt-7 " >
-        <button className="text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none flex hover:bg-pink-600 rounded text-lg w-[70%]  items-center text-center justify-center ">Clear Cart</button>
-        <button className="text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none flex hover:bg-pink-600 rounded text-lg w-[70%]  items-center text-center justify-center ">Check out<AiOutlineArrowRight className="mx-2 mt-1 " /></button>
-        </div>
+				<div className="flex justify-center items-center flex-col space-y-4 sm:space-y-0 sm:space-x-3 sm:flex-row mt-7 ">
+					<button
+						onClick={props.clearCart}
+						className="text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none flex hover:bg-pink-600 rounded text-lg w-[70%]  items-center text-center justify-center "
+					>
+						Clear Cart
+					</button>
+					<button className="text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none flex hover:bg-pink-600 rounded text-lg w-[70%]  items-center text-center justify-center ">
+						Check out
+						<AiOutlineArrowRight className="mx-2 mt-1 " />
+					</button>
+				</div>
 			</div>
 		</header>
 	);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,120 @@
-import Footer from '@/components/Footer'
-import NavBar from '@/components/NavBar'
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
+import Footer from "@/components/Footer";
+import NavBar from "@/components/NavBar";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import React from "react";
+import { useEffect, useState } from "react";
+
+interface ProductCart {
+	itemCode: number;
+	qty: number;
+	price: number;
+	name: string;
+	size: string;
+	variant: string;
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-       <Head>
-        <title>Next-ECommerce</title>
-        <meta property="og:title" content="My page title" key="title" />
-      </Head>
-      <NavBar/>
+	const [cart, setCart] = useState<{ [itemCode: number]: ProductCart }>({});
+	const [subTotal, setSubTotal] = useState(0);
 
-      <Component {...pageProps} />
-      <Footer/>
-    </>
-  )
+	useEffect(() => {
+		// console.log("hey")
+		try {
+			const savedCart = localStorage.getItem("cart");
+			if (savedCart !== null) {
+				setCart(JSON.parse(savedCart));
+			} else {
+			}
+		} catch (error) {
+			console.error(error);
+			localStorage.clear();
+		}
+	}, []);
+
+	const saveCart = (myCart: any) => {
+		localStorage.setItem("cart", JSON.stringify(myCart));
+		let subT = 0;
+		let keys = Object.keys(myCart);
+		for (let i = 0; i<keys.length; i++) {
+			subT += myCart[keys[i]].price * myCart[keys[i]].qty;
+		}
+    setSubTotal(subT)
+	};
+
+	const addToCart = (
+		itemCode: number,
+		qty: number,
+		price: number,
+		name: string,
+		size: string,
+		variant: string
+	): void => {
+		let newCart = cart;
+		if (itemCode in cart) {
+			newCart[itemCode].qty = cart[itemCode].qty + qty;
+		} else {
+			newCart[itemCode] = {
+				qty: 1,
+				price,
+				name,
+				size,
+				variant,
+				itemCode,
+			};
+		}
+		setCart(newCart);
+		saveCart(newCart);
+	};
+
+	const clearCart = () => {
+		setCart({});
+		saveCart({});
+	};
+
+	const removeFromCart = (
+		itemCode: number,
+		qty: number,
+		price: number,
+		name: string,
+		size: string,
+		variant: string
+	): void => {
+		let newCart = cart;
+		if (itemCode in cart) {
+			newCart[itemCode].qty = cart[itemCode].qty - qty;
+		}
+		if (newCart[itemCode]["qty"] <= 0) {
+			delete newCart[itemCode];
+		}
+		setCart(newCart);
+		saveCart(newCart);
+	};
+
+	return (
+		<>
+			<Head>
+				<title>Next-ECommerce</title>
+				<meta property="og:title" content="My page title" key="title" />
+			</Head>
+			<NavBar
+				cart={cart}
+				removeFromCart={removeFromCart}
+				addToCart={addToCart}
+				subTotal={subTotal}
+				clearCart={clearCart}
+			/>
+
+			<Component
+				cart={cart}
+				removeFromCart={removeFromCart}
+				addToCart={addToCart}
+				subTotal={subTotal}
+				clearCart={clearCart}
+				{...pageProps}
+			/>
+			<Footer />
+		</>
+	);
 }
